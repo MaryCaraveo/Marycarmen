433db579de9af603c4e6e3d98bd18f2264efcdb5
Mary Caraveo <marykravo07@gmail.com>
2025-04-07 14:10:14 -0600

    Add files via upload

---
+++ ejemplo.js
@@ -0,0 +1,12 @@
+function saludar()
+{
+    let saludar1= prompt("Escribe tu nombre");
+    alert("Hola "+saludar1+" suerte en tu examen");
+}
+
+function calcularCuadrado()
+{
+    let numcuadrado=parseFloat(prompt("Ingresa un número"));
+    let cuadrado= (numcuadrado*numcuadrado);
+    alert("El cuadrado del número es "+ cuadrado);
+}
